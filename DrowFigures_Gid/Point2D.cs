3389b70b9cd4735ebece569daeb347027e949c17
GGzloi96 <kenackiken@yandex.ru>
2026-09-02 14:05:56 +0700

    Написал класс Point2D

---
+++ DrowFigures_Gid/Point2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrowFigures_Gid
+{
+    public class Point2D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void AddX(int x)
+        {
+            X += x;
+        }
+
+        public void AddY(int y)
+        {
+            Y += y;
+        }
+
+    }
+}
