43879d09ec61e9b0d8bab813fd9297cebfeda717
GGzloi96 <kenackiken@yandex.ru>
2026-09-02 14:33:59 +0700

    Добавил холст Canvas, Добавил функции для работы с холстом, Добавил функцию для отрисовки треугольника

--- DrowFigures_Gid/MainWindow.xaml.cs
+++ DrowFigures_Gid/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace DrowFigures_Gid
     /// </summary>
     public partial class MainWindow : Window
     {
+        Triangle tr;
+        Random rnd = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            Point2D P1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int) Scene.Height));
+            Point2D P2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D P3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+
+            tr = new Triangle(P1, P2, P3);
         }
+
+        public void DrawLine(Point2D p1, Point2D p2)
+        {
+            
+            Line line = new Line();
+            
+            line.Stroke = Brushes.Red;
+            line.StrokeThickness = 3;
+            
+            line.X1 = p1.X;
+            line.Y1 = p1.Y;
+            line.X2 = p2.X;
+            line.Y2 = p2.Y;
+            
+            Scene.Children.Add(line);
+        }
+
+        public void DrawTriangle(Triangle tr)
+        {
+            
+            DrawLine(tr.P1, tr.P2);
+            DrawLine(tr.P2, tr.P3);
+            DrawLine(tr.P3, tr.P1);
+        }
+
+        public void ClearScene()
+        {
+            
+            Scene.Children.Clear();
+        }
+
+
+
+
     }
 }
